ebc05e1922701f0704919b252a5e83607e4ef0c6
NaokiMizuta5 <naoki.mizuta.work@gmail.com>
2023-09-08 15:35:12 +0000

    [ADD] added post and delete functions and views without edit.html

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+    
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def destroy 
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed" 
+      redirect_to '/books'
+    else 
+      flash.now[:alert] = "Book was not successfully destroyed"
+      render :index
+    end
+  end
+  
+  private 
+  def book_params 
+    params.require(:book).permit(:title, :body)
+  end
+end
